6815350aed581ed1c8f7b86627282e9dd90c52a7
TamyahD <tamyahdownes@yahoo.com>
2021-08-30 18:47:04 -0400

    restructured header

--- assets/javascript/mobile_menu.js
+++ assets/javascript/mobile_menu.js
@@ -1,17 +1,10 @@
 // select DOM items
 const menuContainer = document.querySelector('div.hmbgr-menu');
-const navMenu = document.querySelector('.nav-menu');
-const navItems = document.querySelectorAll('.nav-item');
-const header = document.querySelector('header');
 const navContainer = document.querySelector('.nav-container');
 
 menuContainer.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     navContainer.classList.toggle('show');
-    navMenu.classList.toggle('show');
-    menuContainer.classList.toggle('show');
-    header.classList.toggle('color');
-
 
 }
