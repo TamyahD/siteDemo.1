49a83ca30e7818d8f389cf8459bef268af16a568
TamyahD <tamyahdownes@yahoo.com>
2021-08-30 23:13:06 -0400

    modified header

--- assets/javascript/mobile_menu.js
+++ assets/javascript/mobile_menu.js
@@ -1,10 +1,12 @@
 // select DOM items
 const menuContainer = document.querySelector('div.hmbgr-menu');
 const navContainer = document.querySelector('.nav-container');
+const header = document.querySelector('.header');
 
 menuContainer.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     navContainer.classList.toggle('show');
+    header.classList.toggle('show');
 
 }
