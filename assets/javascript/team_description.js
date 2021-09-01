c399a91b16f44feebef398b08dbebd6b4d910496
TamyahD <tamyahdownes@yahoo.com>
2021-09-01 17:12:25 -0400

    animated team content

--- assets/javascript/team_description.js
+++ assets/javascript/team_description.js
@@ -1,2 +1,10 @@
-const memberName = document.querySelector('.member-name');
-const description = document.querySelector('.member-description');
+const memberName = document.querySelector('p.name');
+const description = document.querySelector('p.description');
+const descriptionWrap = document.querySelector('div.description-wrap');
+memberName.addEventListener('click', revealDescription);
+
+function revealDescription() {
+    console.log('Name successfully clicked!');
+    // descriptionWrap.classList.toggle('reveal');
+    // description.classList.toggle('reveal');
+}
