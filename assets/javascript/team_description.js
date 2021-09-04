e6af71e16108029c0332970e6850d61161ce0521
TamyahD <tamyahdownes@yahoo.com>
2021-09-03 20:06:30 -0400

    merged proper aboutPg animations with master

--- assets/javascript/team_description.js
+++ assets/javascript/team_description.js
@@ -1,2 +1,36 @@
-const memberName = document.querySelector('.member-name');
-const description = document.querySelector('.member-description');
+// target click elements (image icons)
+const images = document.querySelectorAll('.img');
+// target memeber name
+const memberName = document.querySelector('.name');
+// target description wrap & description
+const descriptionWrap = document.querySelector('.description-wrap');
+const description = document.querySelector('.description');
+
+// select all image icons, add event listener & function to 
+// each icon.
+// console log id of image targeted/clicked
+
+// if id matches specified id, style that specific
+// member's elements
+// verify specific images clicked+
+// if certain image clicked+
+// style that specific section+
+images.forEach(image => {
+    image.addEventListener('click', (e) => {
+        // console.log(e.target.id);
+
+        if (e.target.id == 'img1') {
+            document.querySelector('p#name1').classList.toggle('disappear');
+            document.querySelector('div#dw1').classList.toggle('show');
+        }
+        else if (e.target.id == 'img2') {
+            document.querySelector('p#name2').classList.toggle('disappear');
+            document.querySelector('div#dw2').classList.toggle('show');
+
+        }
+        else {
+            document.querySelector('p#name3').classList.toggle('disappear');
+            document.querySelector('div#dw3').classList.toggle('show');
+        }
+    });
+})
